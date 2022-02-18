b09546c4fc1b48b701a47a5ffbd3b4fafb7076fb
Monako <poi.nyaa@qq.com>
2022-02-18 17:52:11 +0800

    Add files via upload

---
+++ components/wave-circle/examples/bg.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { WaveCircle } from 'skyline-ui';
+
+const Example: React.FC = () => {
+  return (
+    <div style={{ padding: 100 }}>
+      <WaveCircle bgColor="pink" />
+    </div>
+  );
+};
+
+export default Example;
