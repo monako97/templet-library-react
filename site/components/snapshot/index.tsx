ce6834c51f31a1d87007fa15a78d776a6662ead2
Monako <poi.nyaa@qq.com>
2022-02-23 16:49:47 +0800

    Update index.tsx

--- site/components/snapshot/index.tsx
+++ site/components/snapshot/index.tsx
@@ -1,37 +1,31 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import CodeBlock from '../code';
 import styles from './index.less';
 
 const Snapshot: React.FC<{ path: string; lang: string }> = ({ path, lang }) => {
-  const [view, setView] = useState<React.ReactElement>();
   const [code, setCode] = useState<string | null>();
-
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  const view = require('@pkg/' + path).default;
   const handleShowSource = useCallback(() => {
     if (code) {
       setCode(null);
     } else {
+      // eslint-disable-next-line @typescript-eslint/no-var-requires
       setCode(require('@pkg/' + path + '?raw'));
     }
   }, [code, path]);
 
-  useEffect(() => {
-    const v = require('@pkg/' + path).default;
-
-    setView(v);
-  }, [path]);
-
   return (
     <div className={styles.snapshot}>
       <div className={styles.preview}>
-        {view}
+        <div className={styles.view}>{React.createElement(view)}</div>
         <div className={[styles.toolbar, code && styles.open].join(' ')} onClick={handleShowSource}>
           {code ? 'hide' : 'show'} source
         </div>
       </div>
       {code && (
         <div className={styles.code}>
-          <CodeBlock code={code} lang={lang} />
+          <CodeBlock code={code as string} lang={lang} />
         </div>
       )}
     </div>
