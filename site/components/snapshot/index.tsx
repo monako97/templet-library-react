3c9c54a307fd917f29b747f5eddf0f92e0385d7f
monako97 <poi.nyaa@qq.com>
2022-03-16 18:30:12 +0800

    refactor：update

--- site/components/snapshot/index.tsx
+++ site/components/snapshot/index.tsx
@@ -1,33 +1,65 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 import React, { useCallback, useState } from 'react';
 import CodeBlock from '../code';
 import styles from './index.less';
 
-const Snapshot: React.FC<{ path: string; lang: string }> = ({ path, lang }) => {
+const Snapshot: React.FC<{ path: string; lang: string; style?: string }> = ({
+  path,
+  lang,
+  style,
+}) => {
   const [code, setCode] = useState<string | null>();
-  // eslint-disable-next-line @typescript-eslint/no-var-requires
-  const view = require('@pkg/' + path).default;
+  const [styleCode, setStyleCode] = useState<string | null>();
+  const isHtml = lang === 'html';
+  const view = require('@pkg/' + path);
+
   const handleShowSource = useCallback(() => {
     if (code) {
       setCode(null);
     } else {
-      // eslint-disable-next-line @typescript-eslint/no-var-requires
+      setStyleCode(null);
       setCode(require('@pkg/' + path + '?raw'));
     }
   }, [code, path]);
+  const handleShowStyle = useCallback(() => {
+    if (styleCode) {
+      setStyleCode(null);
+    } else {
+      setCode(null);
+      setStyleCode(require('@pkg/' + style + '?raw'));
+    }
+  }, [style, styleCode]);
 
   return (
     <div className={styles.snapshot}>
       <div className={styles.preview}>
-        <div className={styles.view}>{React.createElement(view)}</div>
+        <div className={styles.view}>
+          {isHtml ? (
+            <div
+              dangerouslySetInnerHTML={{
+                __html: view,
+              }}
+            />
+          ) : (
+            React.createElement(view.default)
+          )}
+        </div>
+        {style && (
+          <div
+            className={[styles.toolbar, styleCode && styles.open].join(' ')}
+            onClick={handleShowStyle}
+          >
+            {styleCode ? 'hide' : 'show'} style
+          </div>
+        )}
         <div className={[styles.toolbar, code && styles.open].join(' ')} onClick={handleShowSource}>
           {code ? 'hide' : 'show'} source
         </div>
       </div>
-      {code && (
-        <div className={styles.code}>
-          <CodeBlock code={code as string} lang={lang} />
-        </div>
-      )}
+      <div className={styles.code}>
+        {code && <CodeBlock code={code as string} lang={lang} />}
+        {styleCode && <CodeBlock code={styleCode as string} lang="css" />}
+      </div>
     </div>
   );
 };
