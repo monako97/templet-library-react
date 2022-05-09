2fd34e4ea2d79f83649b205379e2f641c7d1e05b
monako97 <poi.nyaa@qq.com>
2022-05-10 01:10:26 +0800

    update

--- site/pages/index.tsx
+++ site/pages/index.tsx
@@ -1,17 +1,32 @@
 import React, { useEffect, useRef } from 'react';
 import { projectBasicInfo, useLocation, useOutlet } from 'plugin-runtime';
 import styles from './index.less';
-import { ConfigProvider } from 'antd';
+import { ConfigProvider, BackTop } from 'antd';
 import zh_CN from 'antd/es/locale/zh_CN';
 import Sider from '../components/sider';
-import { BackTop } from 'react-hooks-widget';
+import Coverage from '@/components/coverage';
 
 ConfigProvider.config(projectBasicInfo.providerConfig);
+
+// const projectCoverage = projectBasicInfo.coverage[projectBasicInfo.programInfo.name] || {};
+
+// type CoverageType = 'statements' | 'conditionals' | 'methods';
+// const conf: Record<CoverageType, string> = {
+//   statements: '语句覆盖率',
+//   conditionals: '条件覆盖率',
+//   methods: '函数覆盖率',
+// };
 
 const App: React.FC = () => {
   const box = useRef<HTMLElement>(null);
   const readme = useOutlet();
   const location = useLocation();
+
+  // const coverage = useMemo(() => {
+  //   const compCoverage = projectBasicInfo.coverage[location.pathname.substring(1)] || {};
+
+  //   return readme === null ? projectCoverage : compCoverage;
+  // }, [location.pathname, readme]);
 
   useEffect(() => {
     box.current?.scrollTo({ top: 0, behavior: 'smooth' });
@@ -24,6 +39,39 @@ const App: React.FC = () => {
         <article className={styles.container}>
           <main>
             <article className={styles.readme} ref={box}>
+              <Coverage />
+              {/* <div className={styles.coverage}>
+                {Object.keys(conf).map((k) => {
+                  const cover = coverage[k],
+                    covered = coverage[`covered${k}`],
+                    coverNum = Math.round((covered / cover) * 100) || 0;
+
+                  let stat = 'success';
+
+                  if (coverNum < 80) {
+                    stat = 'warning';
+                  }
+                  if (coverNum < 50) {
+                    stat = 'error';
+                  }
+
+                  return (
+                    <div key={k} className={`${styles.item} ${styles[stat]}`}>
+                      <div className={styles.label}>{conf[k as CoverageType]}</div>
+                      <div className={styles.value}>
+                        {cover ? (
+                          <React.Fragment>
+                            <div>{coverNum}%</div>
+                            <div>{`${cover} / ${covered}`}</div>
+                          </React.Fragment>
+                        ) : (
+                          <strong>0%</strong>
+                        )}
+                      </div>
+                    </div>
+                  );
+                })}
+              </div> */}
               {readme}
             </article>
             <BackTop target={() => box.current || document.body} />
