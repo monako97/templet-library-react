bf6c7e0bc1950d972739bfbf9622591051d19c4d
moneko97 <poi.nyaa@qq.com>
2024-10-09 05:30:32 +0800

    update

--- components/card/index.tsx
+++ components/card/index.tsx
@@ -23,19 +23,20 @@ export interface CardProps {
 }
 
 /** 对于字面量建议使用枚举 */
-enum Size {
+export enum Size {
   /** 小 */
   small,
   /** 默认 */
   normal,
 }
 /** 类型 */
-enum Type {
+export enum Type {
   /** 线 */
   line = 'line',
   /** 卡片 */
   card = 'card',
 }
+
 function Card(props: CardProps) {
   return (
     <div>
